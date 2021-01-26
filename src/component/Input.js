ebff14fa6244e5cb020d93777b7406381c5a6984
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-01-26 22:17:52 +0530

    'push-input-value'

--- src/component/Input.js
+++ src/component/Input.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Input() {
+    const [text,setText] =useState('')
+    const [list, setList] = useState([])
     return (
         <div className='input'>
-            <input type="text" placeholder="add details"/>
-            <button>Add</button>
+            <input type="text" placeholder="add details" onChange={createText} value={text}/>
+            <button onClick={updateList}>Add</button>
+
+            {list.map((list)=> <li>{list}</li>)}
         </div>
     )
+
+    function createText(e) {
+        setText(e.target.value)
+    }
+    function updateList(){
+        console.log("List",list,"Text",text)
+        setList((prevState) => [...prevState,list.push(text)]);
+        setText('')
+        console.log("List",list,"Text",text)
+    }
 }
 
 export default Input
