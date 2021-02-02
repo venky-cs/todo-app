c3acb90bab1563151eaebd5c9e5d1e3c68a8cb95
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-02-02 22:44:56 +0530

    'fix-rendering-data'

--- src/component/List.js
+++ src/component/List.js
@@ -1,11 +1,15 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 
 function List() {
     let data = JSON.parse(localStorage.getItem('todo')) || []
     console.log(data)
+    const [state,setState] =useState([])
+    useEffect(() => {
+        setState(data)
+    }, [state])
     return (
         <div>
-            {data.map((data) => <div className='list' onClick={(e) => console.log(e.target.innerHTML)}>
+            {state.map((data) => <div className='list' onClick={(e) => console.log(e.target.innerHTML)}>
             <input type='checkbox' checked={data.complete}/>
                 <label style={data.complete ?{textDecoration:'line-through'} :null}>{data.value}</label>
                 </div>
