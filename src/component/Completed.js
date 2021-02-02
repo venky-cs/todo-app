7e93c6303a87ce0fe3f2100e2b26672c6039ccaf
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-02-02 22:17:13 +0530

    'create-complete-filter'

--- src/component/Completed.js
+++ src/component/Completed.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Completed() {
+    let data = JSON.parse(localStorage.getItem('todo')) || []
+    console.log(data)
+    let complete = data.filter((data)=> data.complete === true)
+    console.log(complete)
     return (
         <div>
-            completed
+            {complete.map((data)=> <li>{data.value}</li>)}
         </div>
     )
 }
