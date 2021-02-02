5e099eb37a500f537574a3566a86c5883f75d8a1
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-02-02 22:22:04 +0530

    'update-active'

--- src/component/Active.js
+++ src/component/Active.js
@@ -2,9 +2,17 @@ import React from 'react'
 import Input from './Input'
 
 function Active() {
+    let data = JSON.parse(localStorage.getItem('todo')) || []
+    console.log(data)
+    let complete = data.filter((data) => data.complete === false)
+    console.log(complete)
     return (
         <div>
             <Input />
+            {complete.map((data) => <div className='list' onClick={(e) => console.log(e.target.innerHTML)}>
+                <input type='checkbox' checked={data.complete} />
+                <label style={data.complete ? { textDecoration: 'line-through' } : null}>{data.value}</label>
+            </div>)}
         </div>
     )
 }
