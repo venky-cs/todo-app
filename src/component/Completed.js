d8d9e1f24ace913cbc56a608cd4918d2c891bc08
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-03-16 19:30:14 +0530

    'add-material-icon'

--- src/component/Completed.js
+++ src/component/Completed.js
@@ -1,35 +1,48 @@
-import React,{useContext} from 'react'
-import { TodoContext } from './Main'
+import React, { useContext } from 'react';
+import { TodoContext } from './Main';
 
-function Completed({test}) {
-    const context = useContext(TodoContext)
-    let contextFilter = context.filter((data) => data && data.complete === true)
-    return (
-        <div>
-            {contextFilter.map((data) => <div className='list' onClick={(e) => (console.log(e.target.innerHTML))}>
-                <input type='checkbox' checked={data && data.complete} />
-                <label style={data && data.complete ? { textDecoration: 'line-through' } : null}>{data && data.value}</label>
-                <button className="delBtn" id={data && data.id} onClick={(e) => {
-                    let data = JSON.parse(localStorage.getItem('todo'))
-                    let value = e.target.id;
-                    // data[value] = data.pop();
-                    const newTask=[]
-                    data.map(data => {
-                        let id = data.id;
-                        if(id != value){
-                            newTask.push(data)
-                        }
-                    })
-                    localStorage.setItem('todo', JSON.stringify(newTask))
-                    test()
-                }}>Delete</button>
-            </div>
-            )}
-            {/* <button className="delBtn" onClick={() =>{
+function Completed({ test }) {
+  const context = useContext(TodoContext);
+  let contextFilter = context.filter((data) => data && data.complete === true);
+  return (
+    <div>
+      {contextFilter.map((data) => (
+        <div className="list" onClick={(e) => console.log(e.target.innerHTML)}>
+          <input type="checkbox" checked={data && data.complete} />
+          <label
+            style={
+              data && data.complete ? { textDecoration: 'line-through' } : null
+            }
+          >
+            {data && data.value}
+          </label>
+          <button
+            className="delBtn material-icons"
+            id={data && data.id}
+            onClick={(e) => {
+              let data = JSON.parse(localStorage.getItem('todo'));
+              let value = e.target.id;
+              // data[value] = data.pop();
+              const newTask = [];
+              data.map((data) => {
+                let id = data.id;
+                if (id != value) {
+                  newTask.push(data);
+                }
+              });
+              localStorage.setItem('todo', JSON.stringify(newTask));
+              test();
+            }}
+          >
+            delete_outline
+          </button>
+        </div>
+      ))}
+      {/* <button className="delBtn" onClick={() =>{
             localStorage.removeItem('todo')
             test()}}>Delete All</button> */}
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Completed
+export default Completed;
